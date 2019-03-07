1053f090ff144a7141e4d49b2e62175a9c149175
bigcheng <big_cheng@asus.com>
2019-03-07 08:57:22 +0800

    add tf client

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = [
+    'grpcio',
+    'grpc',
+    'tensorflow',
+    'tensorflow-serving-api'
+]
+
 setuptools.setup(
     name="frapi",
-    version="0.0.1",
+    version="0.0.2",
     author="BIG CHENG",
     author_email="bigcheng.asus@gmail.com",
     description="Package for Face Recognition API",
